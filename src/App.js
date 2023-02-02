78e339b5f46c2de197a6f6c8e853a3844bf87360
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-02-02 21:47:47 +0600

    added a row

--- src/App.js
+++ src/App.js
@@ -3,7 +3,53 @@ import React from "react";
 const App = () => {
   return (
     <div className="App">
-      <h2 className="text-center">Hi man</h2>
+      <center>
+        <table class="table w-[32rem] border-2 bg-gray-50 rounded-lg m-8 p-11">
+          <thead>
+            <tr>
+              <th></th>
+              <th></th>
+              <th></th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody className="text-center">
+            <tr>
+              <td>my name</td>
+              <td>
+                <div>
+                  <h2 className="font-semibold">Contact</h2>
+                </div>
+                <div>
+                  <p>Rakibul</p>
+                </div>
+              </td>
+              <td>
+                <div>
+                  <h2 className="font-semibold">City</h2>
+                </div>
+                <div>
+                  <p>Dhaka</p>
+                </div>
+              </td>
+              <td>
+                <div>
+                  <h2 className="font-semibold">State</h2>
+                </div>
+                <div>
+                  <p>Bangladesh</p>
+                </div>
+              </td>
+              <td>
+                <button className="bg-orange-600 px-2 py-1 rounded-md text-white">
+                  view Detail
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </center>
     </div>
   );
 };
